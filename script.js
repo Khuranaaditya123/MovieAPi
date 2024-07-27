6241439a11b3a79517eb375bf95511a25c0376ea
Khuranaaditya123 <112614814+Khuranaaditya123@users.noreply.github.com>
2024-07-28 01:10:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ae452f5050dbd11e72aa63c5f87c0448&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=ae452f5050dbd11e72aa63c5f87c0448&query=';
+
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
+
+returnMovies(APILINK)
+function returnMovies(url){
+    fetch(url).then(res => res.json())
+    .then(function(data){
+    console.log(data.results);
+    data.results.forEach(element => {
+        const div_card = document.createElement('div');
+        div_card.setAttribute('class','card');
+        const div_row = document.createElement('div');
+        div_row.setAttribute('class','row');
+        const div_column = document.createElement('div');
+        div_column.setAttribute('class','column');
+        const image = document.createElement('img');
+        image.setAttribute('class','thumbnail');
+        image.setAttribute('id','image');
+        const title = document.createElement('h3');
+        title.setAttribute('id','title');
+        const center = document.createElement('center');
+
+        title.innerHTML = `${element.title}`; 
+        image.src = IMG_PATH + element.poster_path;
+
+        center.appendChild(image);
+        div_card.appendChild(center);
+        div_card.appendChild(title);
+        div_column.appendChild(div_card);
+        div_row.appendChild(div_column);
+
+        main.appendChild(div_row);
+        });
+    });
+}
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    main.innerHTML = '';
+
+    const seachItem = search.value;
+
+    if(seachItem){
+        returnMovies(SEARCHAPI + seachItem);
+        search.value = '';
+    }
+})
